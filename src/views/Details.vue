b94cbff7c7561d703f57a7dff6916dc84e440a6c
hana <hana@HanaHaru.local>
2021-07-06 11:45:30 +0900

    adding delete function

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,30 +1,40 @@
 
 <template>
+<div>
   <div v-if="error">{{ error }}</div>
   <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
+    <button @click="handleClick" class="delete">delete post</button>
   </div>
   <div v-else>
     <Spinner />
   </div>
+</div>
 </template>
 
 <script>
 import getPost from '../composables/getPost'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 // component imports
 import Spinner from '../components/Spinner.vue'
+import { projectFirestore } from '../firebase/config'
 export default {
   props: ['id'],
   components: { Spinner },
   setup(props) {
     const route = useRoute()
+    const router= useRouter()
     // console.log(route)
     // console.log(route.params)
     const { error, post, load } = getPost(route.params.id)
     load()
-    return { error, post }
+    const handleClick = async ()=>{
+      await projectFirestore.collection('posts').doc(props.id).delete()
+
+      router.push({name: 'Home' })
+    }
+    return { error, post, handleClick }
   },
 }
 </script>
@@ -41,5 +51,8 @@ export default {
   }
   .pre {
     white-space: pre-wrap;
+  }
+  button.delete{
+    margin: 10px auto;
   }
 </style>
